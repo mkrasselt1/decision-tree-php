9d7d6bf5faf91bfc6cc7cb153a26bf534fdacd36
Michael Krasselt <michael@peanutpay.de>
2025-06-23 11:23:32 +0200

    Refactor Eingabevalidierung und Initialisierung der Variablen in edit.php

--- edit.php
+++ edit.php
@@ -3,15 +3,24 @@ include 'config.php';
 include 'functions.php';
 include 'prepare.php';
 
-/*echo "<pre>";
-print_r($_POST);
-echo "</pre>";*/
-$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT, ["options" => ["default" => null, "min_range" => 0, "max_range" => 9999999999]]);
-$text = filter_input(INPUT_POST, 'text', FILTER_UNSAFE_RAW, ["options" => ["default" => null]]);
-$titel = filter_input(INPUT_POST, 'titel', FILTER_UNSAFE_RAW, ["options" => ["default" => null]]);
-$frage = filter_input(INPUT_POST, 'frage', FILTER_UNSAFE_RAW, ["options" => ["default" => null]]);
-$idChain = filter_input(INPUT_GET, 'idChain', FILTER_VALIDATE_REGEXP, ["options" => ["default" => '0', "regexp" => "/^\d+(-{1}\d+)*$/"]]);
-$replace["{id}"] = $replace["{Titel}"] = $replace["{Frage}"] = $replace["{Text}"] = $replace["{lastid}"] = '';
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT, [
+	"options" => ["default" => null, "min_range" => 0, "max_range" => 9999999999]
+]);
+$idChain = filter_input(INPUT_GET, 'idChain', FILTER_VALIDATE_REGEXP, [
+	"options" => ["default" => '0', "regexp" => "/^\d+(-\d+)*$/"]
+]);
+
+$fields = ['text', 'titel', 'frage'];
+foreach ($fields as $field) {
+	$$field = filter_input(INPUT_POST, $field, FILTER_UNSAFE_RAW, [
+		"options" => ["default" => null]
+	]);
+}
+
+$replace = array_fill_keys(
+	['{id}', '{Titel}', '{Frage}', '{Text}', '{lastid}'],
+	''
+);
 $replace["{idChain}"] = $idChain;
 if (isset($_POST["save"])) {
 	if (!is_null($id)) {
